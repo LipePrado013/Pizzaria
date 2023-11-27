e594bf34f2765bc6bc57be934d98ff0e3cfe1a23
LipePrado013 <lipe.praado@gmail.com>
2023-11-27 12:10:43 -0300

    para aula de hj

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Route path="/cardapio" element={<Cardapio />} />
         <Route path="/adm" element={<PedidosAdm />} />
         <Route path="/addpizza" element={<AddPizza />} />
-
       </Routes>
     </BrowserRouter>
   )
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import img from '../assets/img/bgLogin.png'
 
+import axios from 'axios';
+import { createBrowserRouter } from 'react-router-dom';
+
 function Login() {
 
   const [troca, setTroca] = useState(true);
@@ -20,21 +23,44 @@ function Login() {
     csenha: ''
   });
 
+  const [message, setMessage] = useState('');
+
   const valorI = e => setCadastro({ ...cadastro, [e.target.name]: e.target.value })
 
-  const sendMsg = e => {
+  const sendMsg = async e => {
     //nãop deixa a pagina recarregar
     e.preventDefault();
-    console.log(`nome: ${cadastro.nome}`)
-    console.log(`email: ${cadastro.email}`)
-    console.log(`senha: ${cadastro.senha}`)
-    console.log(`consfirma senha: ${cadastro.csenha}`)
+
+
+    const headers = {
+      'headers': {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    await axios.post('http://localhost:8080/message', cadastro, headers).then((response) => {
+      setMessage(response.data.message);
+      setCadastro({
+        nome: '',
+        email: '',
+        senha: '',
+        csenha: ''
+      });
+
+    }).catch((error) => {
+      setMessage(error.response.data.message)
+    })
+
+
   }
 
   return (
     <>
       <div className={`flex justify-center items-center w-screen h-screen bg-[url('${img}')] bg-no-repeat bg-cover`}>
         <div className=" w-[30rem] h-[40rem] fixed flex-col right-[15rem] flex justify-evenly  rounded-[20px] backdrop-blur-sm shadow-[0_0_20px]">
+          {/* {
+            message ? <p>{message}</p> : ''
+          } */}
           <div className=" w-[30rem] rounded-t-lg z-10 p-1 gap-3 flex ml-5">
             <button onClick={() => setTroca(true)} className={`p-2 rounded-lg font-bold transition-all ${troca ? ('bg-blue-500 ') : ('text-white')}`}>Login</button>
             <button onClick={() => setTroca(false)} className={`p-2 rounded-lg font-bold transition-all ${troca ? ('text-white') : ('bg-blue-500 ')}  `}>Cadastro</button>
@@ -44,7 +70,7 @@ function Login() {
             <div className=" w-[30rem] h-[25rem]  p-2 ">
               <form onSubmit={sendMsg} method="get" className="flex flex-col gap-5 items-center">
                 <h1 className='text-5xl text-white font-bold mb-10'>Login</h1>
-                <input type="text" name='email' id='email' placeholder="E-mail" className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" />
+                <input type="text" name='email' id='email' placeholder="E-mail" className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl " />
                 <input type="text" name='senha' id='senha' placeholder="Senha" className="p-1 rounded-lg  w-[15rem] h-[3rem] text-xl" />
                 <button type='submit' className="pt-2 pb-2 pl-10 pr-10 bg-[#12FFA3] text-xl rounded-lg">Logar</button>
               </form>
@@ -54,10 +80,10 @@ function Login() {
             <div className=" w-[30rem] h-[25rem]  p-2 ">
               <form onSubmit={sendMsg} method="get" className="flex flex-col gap-5 items-center">
                 <h1 className='text-5xl text-white font-bold mb-10'>Cadastro</h1>
-                <input type="text" placeholder="Nome" name='nome' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl " onChange={valorI} />
-                <input type="text" placeholder="E-mail" name='email' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} />
-                <input type="text" placeholder="Senha" name='senha' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} />
-                <input type="text" placeholder="Confirmar senha" name='csenha' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} />
+                <input type="text" placeholder="Nome" name='nome' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl " onChange={valorI} value={cadastro.nome} />
+                <input type="text" placeholder="E-mail" name='email' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} value={cadastro.email} />
+                <input type="text" placeholder="Senha" name='senha' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} value={cadastro.senha} />
+                <input type="text" placeholder="Confirmar senha" name='csenha' className="p-1 rounded-lg w-[15rem] h-[3rem] text-xl" onChange={valorI} value={cadastro.csenha} />
                 <button className="pt-2 pb-2 pl-10 pr-10 bg-[#12FFA3] text-xl rounded-lg">Cadastrar</button>
               </form>
             </div>
